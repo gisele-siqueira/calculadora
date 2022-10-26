8c3c3169a8625326992643563b81675d35be1014
Gabriel <92406192+H-Gabriel@users.noreply.github.com>
2022-10-26 21:08:04 +0000

    Implementação das funcionalidades da calculadora

--- cal.js
+++ cal.js
@@ -1,19 +1,21 @@
 function soma(a, b){
-  
+  return a+b;
 }
 function subt(a, b){
-  
+  return a-b;
 }
 function mult(a, b){
-  
+  return a*b;
 }
 function div(a, b){
-  
+  if (b!=0) {
+    return a/b;
+  }
 }
-function piso(a, b){
-  
+function piso(a){
+  return Math.floor(a);
 }
-function teto(a, b){
-  
+function teto(a){
+  return Math.ceil(a)
 }
 
